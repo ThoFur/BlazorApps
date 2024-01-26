59d3b12be640179520048abcd81354ae0c9f7d9a
ThoFur <Thomas.Furniere@vdabcampus.be>
2024-01-26 20:30:33 +0100

    BlazorServerAppCursus

---
+++ BlazorServerApp/Data/Artikel.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorServerApp.Data
+{
+    public class Artikel
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public string Omschrijving { get; set; }
+        public decimal Prijs { get; set; }
+    }
+}
